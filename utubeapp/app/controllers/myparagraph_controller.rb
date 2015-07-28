c99e81f905ce85e7cd66258be73436b789c2f887
freemind13 <mishclash@gmail.com>
2015-07-28 14:34:14 +0300

    fitrst

---
+++ utubeapp/app/controllers/myparagraph_controller.rb
@@ -0,0 +1,20 @@
+class MyparagraphController < ApplicationController
+	def index
+
+	end
+	def show
+
+	end
+	def new
+	
+	end
+	def edit
+	
+	end
+	def update
+	
+	end
+	def destroy
+	
+	end
+end
---
+++ utubeapp/app/helpers/myparagraph_helper.rb
@@ -0,0 +1,2 @@
+module MyparagraphHelper
+end
